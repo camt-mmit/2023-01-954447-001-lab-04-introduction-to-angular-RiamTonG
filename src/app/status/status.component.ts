b424843f3ff5f8c6d14d51a9f63d4ca9eda66b44
Taratip Cheawsinrapatham <taratiptongtong@gmail.com>
2023-07-17 17:29:18 +0700

    week-04

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -6,8 +6,13 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './status.component.html',
-  styleUrls: ['./status.component.scss']
+  styleUrls: ['./status.component.scss'],
 })
 export class StatusComponent {
-
+  currentTime = Date();
+  userAgent = navigator.userAgent;
+  languages = navigator.languages;
+  cookies = navigator.cookieEnabled;
+  touches = navigator.maxTouchPoints;
+  logical = navigator.hardwareConcurrency;
 }
